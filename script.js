ec81c711da7d5623a9825861e781e893ee07508a
Virgil <lunguvirgil3000@gmail.com>
2022-07-10 09:52:41 +0300

    add script js file with setBgToBody, setActiveSlide and rightBtn+leftBtn event listeners

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const openBtn = document.querySelector('.open-btn');
 const closeBtn = document.querySelector('.close-btn');
 const nav = document.querySelectorAll('.nav');
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+let activeSlide = 0;
 
 openBtn.addEventListener('click', () => {
     nav.forEach(nav_el => nav_el.classList.add('visible'))
@@ -9,3 +15,35 @@ openBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     nav.forEach(nav_el => nav_el.classList.remove('visible'))
 })
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++;
+    if(activeSlide > slides.length - 1) {
+        activeSlide = 0;
+    }
+    
+    setBgToBody();
+    setActiveSlide();
+})
+
+leftBtn.addEventListener('click', () => {
+    activeSlide--;
+    if(activeSlide < 0) {
+        activeSlide = slides.length-1;
+    }
+    
+    setBgToBody();
+    setActiveSlide();
+})
+
+setBgToBody()
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;    
+}
+
+function setActiveSlide() {
+    slides.forEach(slide => slide.classList.remove('active'));
+
+    slides[activeSlide].classList.add('active');
+}
